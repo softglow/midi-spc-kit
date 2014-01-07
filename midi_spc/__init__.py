98df97f06adbf28920fe7c0d4adb197198d24657
softglow <da.softglow@gmail.com>
2014-01-06 19:58:51 -0500

    Add a __main__.py for testing the imports.
    Running `python -B -m midi_spc` from the toplevel now yields the version
    number, proving that the basic import runs correctly.

--- midi_spc/__init__.py
+++ midi_spc/__init__.py
@@ -1,2 +1,4 @@
+from __future__ import absolute_import
+from . import config
 __version__ = '0.0.1'
 __all__ = ['config']
---
+++ midi_spc/__main__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import, print_function
+import midi_spc
+
+if __name__ == '__main__':
+    print("midi_spc version {0}".format(midi_spc.__version__))
